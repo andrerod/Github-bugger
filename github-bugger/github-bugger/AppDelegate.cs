c55189588f44a948929b9098f9d4223b158b0dd9
Andre Rodrigues <andrerod@microsoft.com>
2013-07-16 19:24:45 +0100

    mroe

--- github-bugger/github-bugger/AppDelegate.cs
+++ github-bugger/github-bugger/AppDelegate.cs
@@ -8,17 +8,22 @@ namespace githubbugger
 {
 	public partial class AppDelegate : NSApplicationDelegate
 	{
-		MainWindowController mainWindowController;
-
-		public AppDelegate ()
-		{
-		}
+		public AppDelegate () { }
 
 		public override void FinishedLaunching (NSObject notification)
 		{
-			mainWindowController = new MainWindowController ();
-			mainWindowController.Window.MakeKeyAndOrderFront (this);
+			// Construct menu that will be displayed when tray icon is clicked
+			var notifyMenu = new NSMenu();
+			var exitMenuItem = new NSMenuItem("Quit My Application", 
+			                                  (a,b) => { System.Environment.Exit(0); }); // Just add 'Quit' command
+			notifyMenu.AddItem(exitMenuItem);
+
+			// Display tray icon in upper-right-hand corner of the screen
+			var sItem = NSStatusBar.SystemStatusBar.CreateStatusItem(30);
+			sItem.Menu = notifyMenu;
+			sItem.Image = NSImage.FromStream(System.IO.File.OpenRead(
+				NSBundle.MainBundle.ResourcePath + @"/github-bugger.png"));
+			sItem.HighlightMode = true;
 		}
 	}
 }
-
--- github-bugger/github-bugger/Main.cs
+++ github-bugger/github-bugger/Main.cs
@@ -10,16 +10,8 @@ namespace githubbugger
 	{
 		static void Main (string[] args)
 		{
-			NSMenu notifyMenu = new NSMenu("Hello world");
-
-			NSStatusItem sItem = NSStatusBar.SystemStatusBar.CreateStatusItem(30);
-			sItem.Menu = notifyMenu;
-			sItem.Image = NSImage.FromStream(System.IO.File.OpenRead(NSBundle.MainBundle.ResourcePath + @"/notify-icon.icns"));
-			sItem.HighlightMode = true;
-			/*
 			NSApplication.Init ();
 			NSApplication.Main (args);
-			*/
 		}
 	}
 }	
